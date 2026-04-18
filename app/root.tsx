ccf45248c82613a22b2b799a27cb7b1db281d7b0
Sardar-Sadiq <sardarsadiq106@gmail.com>
2026-04-18 12:57:44 +0530

    created

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,11 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+export const meta: Route.MetaFunction = () => [
+  { title: "Vision Training & Placements" },
+  { name: "description", content: "Vision Training & Internship - Engineering the Future of Learning." },
+];
+
 export const links: Route.LinksFunction = () => [
   { rel: "icon", href: "/V.png", type: "image/png" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
